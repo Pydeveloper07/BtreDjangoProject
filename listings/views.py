226c6134a29f4a3662c61b62cfb643d680ca5e49
Tukhtamurod Isroilov <inha07111999@gmail.com>
2020-04-03 15:32:22 +0500

    Searching set up

--- listings/views.py
+++ listings/views.py
@@ -1,6 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Listing
+from realtors.models import Realtor
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+from pages.choices import state_choices, bedroom_choices, price_choices
 
 def index(request):
     listings = Listing.objects.order_by('-list_date').filter(is_published=True)
@@ -15,7 +17,58 @@ def index(request):
     return render(request, 'listings/listings.html', context)
 
 def listing(request, listing_id):
-    return render(request, 'listings/listing.html')
+    listing = get_object_or_404(Listing, pk=listing_id)
+    mvp_realtor = get_object_or_404(Realtor, is_mvp=True)
+    context = {
+        'listing': listing,
+        'mvp_realtor': mvp_realtor,
+    }
+    return render(request, 'listings/listing.html', context)
 
 def search(request):
-    return render(request, 'listings/search.html')
+    queryset_listings = Listing.objects.all()
+    keywords = city = state = ''
+    bedrooms = price = 0
+    #Filtering by keywords
+    if 'keywords' in request.POST:
+        if request.POST['keywords']:
+            keywords = request.POST['keywords']
+            queryset_listings = queryset_listings.filter(description__icontains=keywords)
+
+    #Filtering by city name
+    if 'city' in request.POST:
+        if request.POST['city']:
+            city = request.POST['city']
+            queryset_listings = queryset_listings.filter(city__iexact=city)
+
+    #Filtering by state
+    if 'state' in request.POST:
+        if request.POST['state']:
+            state = request.POST['state']
+            queryset_listings = queryset_listings.filter(state__iexact=state)
+    
+    #Filtering by bedrooms
+    if 'bedrooms' in request.POST:
+        if request.POST['bedrooms']:
+            bedrooms = request.POST['bedrooms']
+            queryset_listings = queryset_listings.filter(bedrooms=bedrooms)
+
+    #Filtering by price
+    if 'price' in request.POST:
+        if request.POST['price']:
+            price = request.POST['price']
+            queryset_listings = queryset_listings.filter(price__lte=price)
+    
+    context = {
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
+        'listings': queryset_listings,
+        'keywords': keywords,
+        'city': city,
+        'state': state,
+        'bedrooms': bedrooms,
+        'price': price,
+
+    }
+    return render(request, 'listings/search.html', context)
--- pages/views.py
+++ pages/views.py
@@ -1,26 +1,18 @@
 from django.shortcuts import render
 from listings.models import Listing
 from realtors.models import Realtor
+from .choices import state_choices, bedroom_choices, price_choices
 
 def index(request):
-    # if request.method == "POST":
-    #     keywords = request.POST.get('keywords')
-    #     city = request.POST.get('city')
-    #     state = request.POST.get('state')
-    #     bedrooms = request.POST.get('bedrooms')
-    #     price = request.POST.get('price')
-    #     listings = Listing.objects.all().filter(city=city, state=state, bedrooms__lte=bedrooms, price__lte=price)
-    #     context = {
-    #         'latest_listings': listings,
-    #     }
-    #     return render(request, 'pages/index.html', context)
-
     try:
         listings = Listing.objects.order_by('-list_date')[:3]
     except:
         listings = Listing.objects.order_by('-list_date')
     context = {
         'latest_listings':listings,
+        'state_choices': state_choices,
+        'bedroom_choices': bedroom_choices,
+        'price_choices': price_choices,
     }
     return render(request, 'pages/index.html', context)
 
